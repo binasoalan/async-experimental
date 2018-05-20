910f18d0c8d9c9568e29b7f1b6333a01094946bd
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-21 00:11:21 +0800

    Refactor using transducer

--- src/clj/binasoalan/handler.clj
+++ src/clj/binasoalan/handler.clj
@@ -20,33 +20,52 @@
   (when (= email "ali@yahoo.com")
     ali))
 
+(def user-existed-msg "Username/email sudah diambil. Sila daftar menggunakan username/email yang lain.")
+(def success-msg "Anda sudah berjaya mendaftar. Sila log masuk .")
+
+(defn- make-flash-msg [[errors data]]
+  (if errors
+    [{:errors errors :data data} data]
+    [nil data]))
+
+(defn- hash-user-password [[errors user :as all]]
+  (if errors
+    all
+    [errors (update user :password hashers/derive)]))
+
+(defn- make-response [[errors _]]
+  (if errors
+    (-> (redirect "/daftar")
+        (flash errors))
+    (-> (redirect "/login")
+        (flash success-msg))))
+
+(def validate-xform
+  (comp
+   (map v/validate-registration)
+   (map make-flash-msg)))
+
+(def register-xform
+  (comp
+   (map hash-user-password)
+   ;; TODO: Add one more function here in the middle to persist user
+   (map make-response)))
+
 (defn register [{:keys [params]} respond _]
-  (let [form (select-keys params [:username :email :password])
-        validated (chan)
-        user (chan)
-        new-user (chan)
-        hashed (chan)
-        result (chan)]
-    (go (>! validated (v/validate-registration form)))
-    (go (let [[errors data] (<! validated)]
-          (if errors
-            (>! result (-> (redirect "/daftar")
-                           (flash {:errors errors
-                                   :data data}))) ; assoc previous data
-            (>! user data))))
-    (go (let [u           (<! user)
-              by-username (async/thread (find-user-by-username (:username u)))
-              by-email    (async/thread (find-user-by-email (:email u)))]
-          (if (or (<! by-username) (<! by-email))
-            (>! result (-> (redirect "/daftar")
-                           (flash {:message "Username/email sudah diambil. Sila daftar menggunakan username/email yang lain."})))
-            (>! new-user u))))
-    (go (>! hashed (update (<! new-user) :password hashers/derive)))
-    (go (<! hashed)
-        (>! result (-> (redirect "/login")
-                       (flash "Anda sudah berjaya mendaftar. Sila log masuk ."))))
-    (go (let [[res _] (alts! [result (timeout 10000)])]
-          (respond res)))))
+  (let [validated (chan 1 validate-xform)
+        result (chan 1 register-xform)]
+    (go (>! validated (select-keys params [:username :email :password])))
+    (go (let [[errors user :as all] (<! validated)]
+          (cond
+            ;; If already has errors, skip checking for existing user.
+            errors (>! result all)
+            ;; Check for existing user with same username or email.
+            (or (<! (async/thread (find-user-by-username (:username user))))
+                (<! (async/thread (find-user-by-email (:email user)))))
+            (>! result [{:message user-existed-msg} user])
+            ;; Return same data if there is no problem.
+            :else (>! result all))))
+    (go (respond (<! result)))))
 
 (defroutes app-routes
   (GET "/" [] views/index)
