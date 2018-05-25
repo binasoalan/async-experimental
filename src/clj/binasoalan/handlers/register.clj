646fb9cd9906370a1d9fd5497dcc3b69b337f410
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-25 15:49:35 +0800

    Close channel after sending response

--- src/clj/binasoalan/handlers/register.clj
+++ src/clj/binasoalan/handlers/register.clj
@@ -8,7 +8,7 @@
             [buddy.core.codecs :as codecs]
             [buddy.core.nonce :as nonce]
             [clojure.core.async :as async :refer [go chan >! <! <!! >!!
-                                                  alts! timeout put!
+                                                  alts! timeout put! close!
                                                   thread take!]]
             [ring.util.response :refer :all]))
 
@@ -22,39 +22,50 @@
 (defn- validate-form [[response-chan form]]
   (let [form-chan (chan)]
     (go
-      (let [[errors valid-form] (v/validate-registration form)]
-        (if errors
-          (>! response-chan (-> (redirect "/daftar")
-                                (flash {:errors errors :data form})))
-          (>! form-chan form))))
+      (let [[errors _] (v/validate-registration form)
+            valid? (not errors)]
+        (if valid?
+          (>! form-chan form)
+          (do
+            (close! form-chan)
+            (>! response-chan (-> (redirect "/daftar")
+                                  (flash {:errors errors :data form})))))))
     [response-chan form-chan]))
 
 (defn- check-existing-user [[response-chan form-chan]]
   (let [user-chan (chan)]
     (go
-      (let [form (<! form-chan)
-            by-username (thread (users/find-user-by-username db-spec form))
-            by-email (thread (users/find-user-by-email db-spec form))
-            already-existed? (or (<! by-username) (<! by-email))]
-        (if already-existed?
-          (>! response-chan (-> (redirect "/daftar")
-                                (flash {:message (:user-existed msg)})))
-          (>! user-chan form))))
+      (if-let [form (<! form-chan)]
+        (let [username-existed (thread (users/find-user-by-username db-spec form))
+              email-existed (thread (users/find-user-by-email db-spec form))
+              available? (not (or (<! username-existed)
+                                  (<! email-existed)))]
+          (if available?
+            (>! user-chan form)
+            (do
+              (close! user-chan)
+              (>! response-chan (-> (redirect "/daftar")
+                                    (flash {:message (:user-existed msg)}))))))
+        (close! user-chan)))
     [response-chan user-chan]))
 
 (defn- persist-user [[response-chan user-chan]]
   (go
-    (let [user (-> (<! user-chan)
-                   (update :password hashers/derive)
-                   (assoc :token (codecs/bytes->hex (nonce/random-bytes 16))))
-          failed? (zero? (<! (thread (users/register-user db-spec user))))]
-      (if failed?
-        (>! response-chan (-> (redirect "/daftar")
-                              (flash {:message (:failed msg)})))
-        (do
-          (future (mailer/send-email-verification user))
-          (>! response-chan (-> (redirect "/login")
-                                (flash {:message (:success msg)}))))))))
+    (when-let [user (<! user-chan)]
+      (let [email-verification-token (codecs/bytes->hex (nonce/random-bytes 16))
+            prepared-user (-> user
+                              (update :password hashers/derive)
+                              (assoc :token email-verification-token))
+            success? (->> (thread (users/register-user db-spec prepared-user))
+                          <!
+                          pos?)]
+        (if success?
+          (do
+            (future (mailer/send-email-verification user))
+            (>! response-chan (-> (redirect "/login")
+                                  (flash {:message (:success msg)}))))
+          (>! response-chan (-> (redirect "/daftar")
+                                (flash {:message (:failed msg)}))))))))
 
 (defn register-handler [{:keys [params] :as req} respond _]
   (let [response-chan (chan)
@@ -64,31 +75,35 @@
            validate-form
            check-existing-user
            persist-user))
-    (go (respond (<! response-chan)))))
+    (go
+      (respond (<! response-chan))
+      (close! response-chan))))
 
 
 ;; Email verification
 
-(defn- check-token [[response-chan token :as param]]
+(defn- check-token [[response-chan token]]
   (let [token-chan (chan)]
     (go
-      (let [not-token? (->> (thread (users/find-token db-spec {:token token}))
-                            <!
-                            :token
-                            nil?)]
-        (if not-token?
-          (>! response-chan (redirect "/login"))
-          (>! token-chan token))))
+      (let [valid? (->> (thread (users/find-token db-spec {:token token}))
+                        <!
+                        :token)]
+        (if valid?
+          (>! token-chan token)
+          (do
+            (close! token-chan)
+            (>! response-chan (redirect "/login"))))))
     [response-chan token-chan]))
 
 (defn- verify [[response-chan token-chan]]
   (go
-    (let [token (<! token-chan)
-          verified? (pos? (<! (thread
-                                (users/verify-user db-spec {:token token}))))]
-      (if verified?
-        (>! response-chan (redirect "/verified"))
-        (>! response-chan (redirect "/login"))))))
+    (when-let [token (<! token-chan)]
+      (let [success? (->> (thread (users/verify-user db-spec {:token token}))
+                          <!
+                          pos?)]
+        (if success?
+          (>! response-chan (redirect "/verified"))
+          (>! response-chan (redirect "/login")))))))
 
 (defn verify-handler [{{token :token} :params :as req} respond _]
   (if (nil? token)
@@ -98,4 +113,6 @@
         (->> [response-chan token]
              check-token
              verify))
-      (go (respond (<! response-chan))))))
+      (go
+        (respond (<! response-chan))
+        (close! response-chan)))))
