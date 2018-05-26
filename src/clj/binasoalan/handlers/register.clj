bc744550ac57d17916df5fca47c679f7a67211b8
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-26 17:08:18 +0000

    Merged in higher-level (pull request #6)
    Use higher level core.async API
    
    * Use higher level core.async API for register handler
    
    * Refactor verify handler to use high level core.async api

--- src/clj/binasoalan/handlers/register.clj
+++ src/clj/binasoalan/handlers/register.clj
@@ -7,9 +7,7 @@
             [buddy.hashers :as hashers]
             [buddy.core.codecs :as codecs]
             [buddy.core.nonce :as nonce]
-            [clojure.core.async :as async :refer [go chan >! <! <!! >!!
-                                                  alts! timeout put! close!
-                                                  thread take!]]
+            [clojure.core.async :as async :refer [go chan alts! put! close!]]
             [ring.util.response :refer :all]))
 
 (def msg {:user-existed "Username/email sudah diambil. Sila daftar menggunakan username/email yang lain."
@@ -19,100 +17,100 @@
 
 ;; User registration
 
-(defn- validate-form [[response-chan form]]
-  (let [form-chan (chan)]
-    (go
-      (let [[errors _] (v/validate-registration form)
-            valid? (not errors)]
-        (if valid?
-          (>! form-chan form)
-          (do
-            (close! form-chan)
-            (>! response-chan (-> (redirect "/daftar")
-                                  (flash {:errors errors :data form})))))))
-    [response-chan form-chan]))
+(defn- check-existing-user [[_ form] out-ch]
+  (->> [(async/thread (users/find-user-by-username db-spec form))
+        (async/thread (users/find-user-by-email db-spec form))]
+       (async/merge)
+       (async/reduce #(boolean (or %1 %2)) false)
+       (async/pipeline 1 out-ch (map #(vector % form)))))
 
-(defn- check-existing-user [[response-chan form-chan]]
-  (let [user-chan (chan)]
-    (go
-      (if-let [form (<! form-chan)]
-        (let [username-existed (thread (users/find-user-by-username db-spec form))
-              email-existed (thread (users/find-user-by-email db-spec form))
-              available? (not (or (<! username-existed)
-                                  (<! email-existed)))]
-          (if available?
-            (>! user-chan form)
-            (do
-              (close! user-chan)
-              (>! response-chan (-> (redirect "/daftar")
-                                    (flash {:message (:user-existed msg)}))))))
-        (close! user-chan)))
-    [response-chan user-chan]))
+(defn- generate-token []
+  (codecs/bytes->hex (nonce/random-bytes 16)))
 
-(defn- persist-user [[response-chan user-chan]]
-  (go
-    (when-let [user (<! user-chan)]
-      (let [email-verification-token (codecs/bytes->hex (nonce/random-bytes 16))
-            prepared-user (-> user
-                              (update :password hashers/derive)
-                              (assoc :token email-verification-token))
-            success? (->> (thread (users/register-user db-spec prepared-user))
-                          <!
-                          pos?)]
-        (if success?
-          (do
-            (future (mailer/send-email-verification user))
-            (>! response-chan (-> (redirect "/login")
-                                  (flash {:message (:success msg)}))))
-          (>! response-chan (-> (redirect "/daftar")
-                                (flash {:message (:failed msg)}))))))))
+(defn- persist-user [[_ user] out-ch]
+  (let [prepared-user (-> user
+                          (update :password hashers/derive)
+                          (assoc :token (generate-token)))]
+    (->> (async/thread (users/register-user db-spec prepared-user))
+         (async/pipeline 1 out-ch (map #(vector (zero? %) prepared-user))))))
+
+(defn- split-if-error [ch]
+  (async/split first ch))
 
 (defn register-handler [{:keys [params] :as req} respond _]
-  (let [response-chan (chan)
-        form (select-keys params [:username :email :password])]
+  (let [input-ch (chan 1 (map v/validate-registration))
+        [invalid-ch valid-ch] (split-if-error input-ch)
+
+        availability-ch (chan)
+        [not-available-ch available-ch] (split-if-error availability-ch)
+
+        persisting-ch (chan)
+        [failed-ch success-ch] (split-if-error persisting-ch)]
+
+    (->> valid-ch
+         (async/pipeline-async 1 availability-ch check-existing-user))
+    (->> available-ch
+         (async/pipeline-async 1 persisting-ch persist-user))
+
     (go
-      (->> [response-chan form]
-           validate-form
-           check-existing-user
-           persist-user))
-    (go
-      (respond (<! response-chan))
-      (close! response-chan))))
+      (let [[val ch] (alts! [invalid-ch not-available-ch failed-ch success-ch])
+            response (condp = ch
+                       invalid-ch
+                       (-> (redirect "/daftar")
+                           (flash {:errors (first val) :data (second val)}))
+
+                       not-available-ch
+                       (-> (redirect "/daftar")
+                           (flash {:message (:user-existed msg)}))
+
+                       failed-ch
+                       (-> (redirect "/daftar")
+                           (flash {:message (:failed msg)}))
+
+                       success-ch
+                       (do
+                         (future (mailer/send-email-verification (second val)))
+                         (-> (redirect "/login")
+                             (flash {:message (:success msg)}))))]
+        (respond response)
+        (close! input-ch)))
+
+    (put! input-ch params)))
 
 
 ;; Email verification
 
-(defn- check-token [[response-chan token]]
-  (let [token-chan (chan)]
-    (go
-      (let [valid? (->> (thread (users/find-token db-spec {:token token}))
-                        <!
-                        :token)]
-        (if valid?
-          (>! token-chan token)
-          (do
-            (close! token-chan)
-            (>! response-chan (redirect "/login"))))))
-    [response-chan token-chan]))
+(defn- check-token [token out-ch]
+  (->> (async/thread (users/find-token db-spec {:token token}))
+       (async/reduce #(boolean (or %1 %2)) false)
+       (async/pipeline 1 out-ch (map #(vector % token)))))
 
-(defn- verify [[response-chan token-chan]]
-  (go
-    (when-let [token (<! token-chan)]
-      (let [success? (->> (thread (users/verify-user db-spec {:token token}))
-                          <!
-                          pos?)]
-        (if success?
-          (>! response-chan (redirect "/verified"))
-          (>! response-chan (redirect "/login")))))))
+(defn- verify [[_ token] out-ch]
+  (->> (async/thread (users/verify-user db-spec {:token token}))
+       (async/pipeline 1 out-ch (map pos?))))
 
-(defn verify-handler [{{token :token} :params :as req} respond _]
-  (if (nil? token)
-    (redirect "/login")
-    (let [response-chan (chan)]
+(defn verify-handler [{:keys [params] :as req} respond _]
+  (if-let [token (:token params)]
+    (let [input-ch (chan)
+
+          validation-ch (chan)
+          [valid-ch invalid-ch] (async/split first validation-ch)
+
+          verification-ch (chan)
+          [success-ch failed-ch] (async/split identity verification-ch)]
+      (->> input-ch
+           (async/pipeline-async 1 validation-ch check-token))
+      (->> valid-ch
+           (async/pipeline-async 1 verification-ch verify))
+
       (go
-        (->> [response-chan token]
-             check-token
-             verify))
-      (go
-        (respond (<! response-chan))
-        (close! response-chan)))))
+        (let [[val ch] (alts! [invalid-ch failed-ch success-ch])
+              response (condp = ch
+                         invalid-ch (redirect "/login")
+                         failed-ch (redirect "/login")
+                         success-ch (redirect "/verified"))]
+          (respond response)
+          (close! input-ch)))
+
+      (put! input-ch token))
+    (respond (redirect "/login"))))
