2759c2b472fd89da3d71c6672d435f166b6286e0
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-05-28 16:17:21 +0800

    Add sample profiles.clj

---
+++ sample.profiles.clj
@@ -0,0 +1,8 @@
+{:profiles/dev
+ {:env {:database-name "binasoalan_dev"
+        :database-username "someencryptedtext"
+        :database-password "someencryptedtext"
+        :email-username "someencryptedtext"
+        :email-password "someencryptedtext"
+        :timbre-level ":debug"
+        :debug "true"}}}
