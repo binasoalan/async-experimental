1722b2eb91f0cb88461f1b618c1742a556692b65
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-06-04 18:14:10 +0800

    Update project.clj to use pedestal

--- project.clj
+++ project.clj
@@ -2,60 +2,51 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
 
-  :min-lein-version "2.7.0"
+  :min-lein-version "2.0.0"
 
-  :dependencies [[buddy "2.0.0"
-                  :exclusions [cheshire
-                               commons-codec]]
-                 [cheshire "5.8.0"]
-                 [cljs-ajax "0.7.3"
-                  :exclusions [commons-codec]]
-                 [com.draines/postal "2.0.2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.pedestal/pedestal.jetty "0.5.3"]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 [org.postgresql/postgresql "42.2.2"]
+                 [org.clojure/java.jdbc "0.7.6"]
                  [com.layerware/hugsql "0.4.8"]
-                 [commons-codec "1.10"]
-                 [compojure "1.6.1"
-                  :exclusions [commons-codec]]
-                 [day8.re-frame/http-fx "0.1.6"
-                  :exclusions [commons-codec]]
-                 [enlive "1.1.6"]
-                 [environ "1.1.0"]
-                 [funcool/struct "1.2.0"]
-                 [hiccup "1.0.5"]
                  [hikari-cp "2.4.0"]
                  [migratus "1.0.6"]
-                 [org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.238"
-                  :exclusions [commons-codec]]
+                 [enlive "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [buddy "2.0.0"]
+                 [com.draines/postal "2.0.2"]
+                 [environ "1.1.0"]
+                 [funcool/struct "1.2.0"]
+                 [ring/ring-core "1.6.3"]
+                 [metosin/ring-http-response "0.9.0"]
                  [org.clojure/core.async "0.4.474"]
-                 [org.clojure/java.jdbc "0.7.6"]
-                 [org.eclipse.jetty/jetty-server "9.4.9.v20180320"]
-                 [org.postgresql/postgresql "42.2.2"]
-                 [org.slf4j/slf4j-simple "1.7.25"]
+                 [cheshire "5.8.0"]
+
+                 [org.clojure/clojurescript "1.10.238"]
+                 [cljs-ajax "0.7.3"]
                  [re-frame "0.10.5"]
                  [reagent "0.8.1"]
-                 [ring "1.6.3"]
-                 [ring/ring-anti-forgery "1.2.0"]
-                 [ring/ring-defaults "0.3.1"]
-                 [ring/ring-json "0.5.0-beta1"]
-                 [secretary "1.2.3"]]
+                 [secretary "1.2.3"]
+                 [day8.re-frame/http-fx "0.1.6"]
+
+                 [ch.qos.logback/logback-classic "1.2.3"
+                  :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.25"]
+                 [org.slf4j/jcl-over-slf4j "1.7.25"]
+                 [org.slf4j/log4j-over-slf4j "1.7.25"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-environ "1.1.0"]
-            [lein-ring "0.12.4"
-             :exclusions [org.clojure/clojure]]]
+            [lein-environ "1.1.0"]]
 
   :source-paths ["src/clj" "src/cljc"]
-  :resource-paths ["resources" "target/resources"]
+  :resource-paths ["config" "resources" "target/resources"]
   :test-paths ["test/clj"]
   :target-path "target/%s"
-  :main ^:skip-aot binasoalan.migrations
+  :main ^:skip-aot binasoalan.server
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
   :auto-clean false
-
-  :ring {:handler binasoalan.core/app
-         :async? true
-         :async-timeout 10000}
 
   :figwheel {:css-dirs ["resources/public/css"]}
 
@@ -86,12 +77,15 @@
 
    :prod {:dependencies [[day8.re-frame/tracing-stubs "0.5.1"]]}
 
+   :uberjar {:aot [binasoalan.server]}
+
    :project/dev {:dependencies [[day8.re-frame/re-frame-10x "0.3.3-react16"]
-                                [day8.re-frame/tracing "0.5.1"]]
+                                [day8.re-frame/tracing "0.5.1"]
+                                [io.pedestal/pedestal.service-tools "0.5.3"]]
 
                  :plugins [[lein-figwheel "0.5.16"
                             :exclusions [org.clojure/clojure]]]}
 
    :project/test {:dependencies [[etaoin "0.2.8-SNAPSHOT"]]}}
 
-  :aliases {"build" ["do" "clean" ["cljsbuild" "once" "min"] ["ring" "uberjar"]]})
+  :aliases {"build" ["do" "clean" ["cljsbuild" "once" "min"] "uberjar"]})
---
+++ src/clj/binasoalan/service.clj
@@ -0,0 +1,32 @@
+(ns binasoalan.service
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.body-params :as body-params]
+            [ring.util.http-response :as resp]))
+
+(defn about-page
+  [request]
+  (resp/ok (format "Clojure %s - served from %s"
+                   (clojure-version)
+                   (route/url-for ::about-page))))
+
+(defn home-page
+  [request]
+  (resp/ok "Hello World!"))
+
+
+(def common-interceptors [(body-params/body-params) http/html-body])
+
+
+(def routes #{["/" :get (conj common-interceptors `home-page)]
+              ["/about" :get (conj common-interceptors `about-page)]})
+
+
+(def service {:env :prod
+              ::http/routes routes
+              ::http/resource-path "/public"
+              ::http/type :jetty
+              ::http/port 8080
+              ::http/container-options {:h2c? true
+                                        :h2? false
+                                        :ssl? false}})
