bf018317e486942e2f4a5680cff95e21c2c299cb
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-06-07 03:26:11 +0800

    Migrate to enlive

--- src/clj/binasoalan/views.clj
+++ src/clj/binasoalan/views.clj
@@ -1,165 +1,101 @@
 (ns binasoalan.views
-  (:require [hiccup.element :refer [link-to]]
-            [hiccup.form :refer [label text-field password-field email-field
-                                 submit-button]]
-            [hiccup.page :refer [html5 include-css include-js]]
-            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+  (:require [net.cgrand.enlive-html :as html]
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]))
 
-(defn header []
-  [:header
-   [:a.logo {:href "#"} "Bina Soalan"]
-   [:a.button {:href "/"} "Laman Utama"]
-   [:a.button {:href "/tentang"} "Tentang Kami"]
-   [:a.button {:href "/login"} "Log Masuk"]])
+(html/defsnippet nav "templates/nav.html" [:nav]
+  [uri]
+  [:ul.navbar-nav [:a (html/attr= :href uri)]] (html/add-class "active"))
 
-(defn nav [& [current-uri]]
-  [:nav.navbar.navbar-default
-   [:div.navbar-header
-    [:button.navbar-toggle.collapsed {:type "button" :data-toggle "collapse"
-                                      :data-target "#navbar-collapse"
-                                      :aria-expanded "false"}
-     [:span.sr-only "Toggle navigation"]
-     [:span.icon-bar]
-     [:span.icon-bar]
-     [:span.icon-bar]]
-    [:span.navbar-brand "Bina Soalan"]]
-   [:div#navbar-collapse.collapse.navbar-collapse
-    [:ul.nav.navbar-nav
-     [:li {:class (if (= current-uri "/") "active" "")}
-      (link-to "/" "Laman Utama")]
-     [:li {:class (if (= current-uri "/tentang") "active" "")}
-      (link-to "/tentang" "Tentang Kami")]
-     [:li {:class (if (= current-uri "/login") "active" "")}
-      (link-to "/login" "Log Masuk")]]]])
+(html/defsnippet login-form "templates/login_form.html" [:form]
+  []
+  [:#anti-forgery-token] (html/set-attr :value *anti-forgery-token*))
 
-(defn base-html
-  [req {:keys [title] :as attr} & contents]
-  (if-not (map? attr)
-    (apply base-html req {} attr contents)
-    (html5
-     [:head
-      [:meta {:charset "utf-8"}]
-      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-      [:title (if title
-                (str title " | Bina Soalan")
-                "Bina Soalan")]
-      (include-css
-       "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-       "css/style.css")]
-     [:body
-      (nav (:uri req))
-      contents
-      (include-js
-       "https://code.jquery.com/jquery-3.3.1.slim.min.js"
-       "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js")])))
+(defn only-when [p transform-fn]
+  (if p
+    transform-fn
+    identity))
 
-(defn login-form []
-  [:form {:method "post" :action "/login"}
-   (anti-forgery-field)
-   [:div.form-group
-    (label "username" "Username")
-    [:input#username.form-control
-     {:type "text" :name "username" :placeholder "Username"}]]
-   [:div.form-group
-    (label "password" "Password")
-    [:input#password.form-control
-     {:type "password" :name "password" :placeholder "Password"}]]
-   [:input.btn.btn-primary.btn-block {:type "submit" :value "Login"}]])
+(defn show-when [p]
+  (when p
+    identity))
 
-(defn register-form [& [{:keys [errors data]}]]
-  [:form {:method "post" :action "/daftar"}
-   (anti-forgery-field)
+(html/defsnippet register-form "templates/register_form.html" [:form]
+  [& [{:keys [errors data]}]]
+  [:#anti-forgery-token] (html/set-attr :value *anti-forgery-token*)
+  [:#username-form-group] (->> (html/add-class "has-error")
+                               (only-when (:username errors)))
+  [:#email-form-group]    (->> (html/add-class "has-error")
+                               (only-when (:email errors)))
+  [:#password-form-group] (->> (html/add-class "has-error")
+                               (only-when (:password errors)))
+  [:#username]            (->> (html/set-attr :value (:username data))
+                               (only-when (:username data)))
+  [:#email]               (->> (html/set-attr :value (:email data))
+                               (only-when (:email data)))
+  [:#username-error]      (show-when (:username errors))
+  [:#email-error]         (show-when (:email errors))
+  [:#password-error]      (show-when (:password errors)))
 
-   [:div.form-group {:class (if (:username errors) "has-error" "")}
-    [:label.control-label {:for "username"} "Username"]
-    [:input#username.form-control
-     {:type "text" :name "username" :placeholder "Username"
-      :value (:username data)}]
-    (when (:username errors)
-      [:span.help-block "Username tidak boleh kosong."])]
+(html/deftemplate base "templates/base.html"
+  [{:keys [uri title content]}]
+  [:head :title] (html/content title)
+  [:header]      (html/content (nav uri))
+  [:#content]    (html/substitute content))
 
-   [:div.form-group {:class (if (:email errors) "has-error" "")}
-    [:label.control-label {:for "email"} "Email"]
-    [:input#email.form-control
-     {:type "text" :name "email" :placeholder "Email" :value (:email data)}]
-    (when (:email errors)
-      [:span.help-block "Email mesti dalam bentuk email yang sah dan tidak boleh kosong."])]
+(html/defsnippet index-content "templates/index_content.html" [:section]
+  []
+  [:#register-form] (html/content (register-form)))
 
-   [:div.form-group {:class (if (:password errors) "has-error" "")}
-    [:label.control-label {:for "password"} "Password"]
-    [:input#password.form-control
-     {:type "password" :name "password" :placeholder "Password"}]
-    (when (:password errors)
-      [:span.help-block "Password tidak boleh kosong."])]
+(html/defsnippet login-content "templates/login_content.html" [:section]
+  [& [{:keys [message] :as errors}]]
+  [:#login-form] (html/substitute (login-form))
+  [:#message]    (when message
+                   (html/content message)))
 
-   [:input.btn.btn-primary.btn-block {:type "submit" :value "Daftar"}]])
+(html/defsnippet daftar-content "templates/daftar_content.html" [:section]
+  [& [{:keys [message] :as errors}]]
+  [:#register-form] (html/substitute (register-form errors))
+  [:#message]       (when message
+                      (html/content message)))
+
+(html/defsnippet verified-content "templates/verified_content.html" [:section]
+  [])
+
+(html/defsnippet tentang-content "templates/tentang_content.html" [:section]
+  [])
 
 
 ;; Pages
 
+
 (defn index
-  ([req]
-   (base-html
-    req
-    [:section.container
-     [:div.col-md-8
-      [:div.jumbotron
-       [:h1 "Bina Soalan"]
-       [:p "I'm bad at copywriting."]]]
-     [:div.col-md-4
-      [:div.panel.panel-default
-       [:div.panel-body
-        (register-form)]]]]))
-  ([req respond _] (respond (index req))))
+  ([request]           (base {:uri (:uri request)
+                              :title "Bina Soalan"
+                              :content (index-content)}))
+  ([request respond _] (respond (index request))))
 
 (defn login
-  ([request] (login request {}))
-  ([request {:keys [message] :as errors}]
-   (base-html
-    request
-    {:title "Log Masuk"}
-    [:section.container
-     [:div.col-md-4.col-md-offset-4
-      (when message
-        [:div.alert.alert-success message])
-      [:div.panel.panel-default
-       [:div.panel-body
-        (login-form)]]]]))
+  ([request]           (login request {}))
+  ([request errors]    (base {:uri (:uri request)
+                              :title "Log Masuk | Bina Soalan"
+                              :content (login-content errors)}))
   ([request respond _] (respond (login request))))
 
 (defn daftar
-  ([request] (daftar request {}))
-  ([request {:keys [message] :as errors}]
-   (base-html
-    request
-    {:title "Daftar"}
-    [:section.container
-     [:div.col-md-6.col-md-offset-3
-      (when message
-        [:div.alert.alert-danger
-         message])
-      (register-form errors)]]))
+  ([request]           (daftar request {}))
+  ([request errors]    (base {:uri (:uri request)
+                              :title "Daftar | Bina Soalan"
+                              :content (daftar-content errors)}))
   ([request respond _] (respond (daftar request))))
 
 (defn verified
-  ([req]
-   (base-html
-    req
-    {:title "Email telah disahkan"}
-    [:section.container
-     [:div.col-md-6.col-md-offset-3
-      [:div.panel.panel-default
-       [:div.panel-body.text-center
-        [:p
-         "Email anda telah disahkan. Anda boleh log in."]
-        [:p [:a {:href "/login"} "Klik di sini untuk log in"]]]]]]))
-  ([req respond _] (respond (verified req))))
+  ([request]           (base {:uri (:uri request)
+                              :title "Email telah disahkan | Bina Soalan"
+                              :content (verified-content)}))
+  ([request respond _] (respond (verified request))))
 
 (defn tentang
-  ([req]
-   (base-html
-    req
-    {:title "Tentang Kami"}
-    [:section
-     [:p "Now that we know who you are, I know who I am."]]))
-  ([req respond _] (respond (tentang req))))
+  ([request]           (base {:uri (:uri request)
+                              :title "Tentang Kami | Bina Soalan"
+                              :content (tentang-content)}))
+  ([request respond _] (respond (tentang request))))
